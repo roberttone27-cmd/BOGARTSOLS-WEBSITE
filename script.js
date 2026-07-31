3b911ceee2d0c1a51bb38c1458155dfdfc514f0a
roberttone27-cmd <roberttone27@gmail.com>
2026-07-31 11:07:03 +0300

    animatii

--- script.js
+++ script.js
@@ -17,4 +17,45 @@ menuToggle.addEventListener("click", function () {
 
     nav.classList.toggle("active");
 
+    if(nav.classList.contains("active")){
+        menuToggle.innerHTML = "✕";
+    }else{
+        menuToggle.innerHTML = "☰";
+    }
+
 });
+
+document.querySelectorAll("nav a").forEach(link => {
+
+    link.addEventListener("click", function(){
+
+        nav.classList.remove("active");
+
+        menuToggle.innerHTML = "☰";
+
+    });
+
+});
+const animatedElements = document.querySelectorAll(".animate");
+
+function revealOnScroll(){
+
+    animatedElements.forEach(element => {
+
+        const elementTop = element.getBoundingClientRect().top;
+
+        const windowHeight = window.innerHeight;
+
+        if(elementTop < windowHeight - 100){
+
+            element.classList.add("show");
+
+        }
+
+    });
+
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+revealOnScroll();
